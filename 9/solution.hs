33f596ab8b608521b840ee3a75b4aa208f422078
Daniel Heurlin <daniel.heurlin@gmail.com>
2020-12-09 17:01:04 +0100

    Solve day 9

---
+++ 9/solution.hs
@@ -0,0 +1,47 @@
+import Data.Foldable
+import Data.Maybe
+import Data.List
+
+-- Checks if a list contains two numbers that add to a given number
+hasSum :: Int -> [Int] -> Bool
+hasSum _ [] = False
+hasSum s (x:xs)
+  | s - x `elem` xs = True
+  | otherwise       = hasSum s xs
+
+-- Returns (head xs) if (tail xs) does *not* contain numbers that sum to (head xs)
+headNotSum :: [Int] -> Maybe Int
+headNotSum (x:xs)
+  | hasSum x xs = Nothing
+  | otherwise   = Just x
+
+-- Returns all sublists of size `size` of xs
+windows :: Int -> [Int] -> [[Int]]
+windows size xs = [ drop n . take (size + n) $ xs | n <- [0..length xs - size] ]
+
+firstNotSum :: Int -> [Int] -> Maybe Int
+firstNotSum preamble xs
+  = asum [ headNotSum (reverse l) | l <- windows (preamble + 1) xs ]
+
+-- The first sequence of xs that adds to s
+seqWithSum :: Int -> [Int] -> Maybe [Int]
+seqWithSum s xs
+  = asum [ initsHasSum s (inits $ drop n xs) | n <- [0 .. length xs] ]
+  where
+    initsHasSum s (xs:xss) = case sum xs of
+      xSum | xSum > s  -> Nothing
+           | xSum == s -> Just xs
+           | otherwise -> initsHasSum s xss
+
+addSeqWithSum :: Int -> [Int] -> Maybe Int
+addSeqWithSum s xs = (\ns -> minimum ns + maximum ns) <$> seqWithSum s xs
+
+main = do
+  putStrLn . ("Star 1: " <>) . show =<< (fromJust <$> firstInvalid)
+  putStrLn . ("Star 2: " <>) . show =<< (fromJust <$> rangeSum)
+    where
+      input        = map read . lines <$> readFile "input"
+      firstInvalid = firstNotSum 25 <$> input
+      rangeSum     = (\sum xs -> (`addSeqWithSum` xs) =<< sum)
+                      <$> firstInvalid <*> input
+
