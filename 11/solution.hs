1cc23507a4f04a4aa35ab95d9bc72d866f6c07e4
Daniel Heurlin <daniel.heurlin@gmail.com>
2020-12-16 00:25:16 +0100

    Solve day 11 part 2

--- 11/solution.hs
+++ 11/solution.hs
@@ -2,74 +2,84 @@
 
 import Debug.Trace
 import Data.Maybe
+import Data.Array
 
-type Grid = [[Char]]
-
-sHead :: [a] -> Maybe a
-sHead [] = Nothing
-sHead xs = Just $ head xs
-
-sLast :: [a] -> Maybe a
-sLast [] = Nothing
-sLast xs = Just $ last xs
+-- type Grid = [[Char]]
+type Grid = Array Int (Array Int Char)
 
 neighbors :: Int -> Int -> Grid ->  [Char]
 neighbors x y g = nRow (y - 1) <> nRow (y + 1) <> nX (x - 1) y <> nX (x + 1) y
   where
-    nX x y | x < 0                    = []
-           | x > length (head g) - 1  = []
-           | otherwise = [g !! y !! x]
+    nX x y | x < 0                   = []
+           | x > length (g ! 0) - 1  = []
+           | otherwise = [g ! y ! x]
     nRow y | y < 0             = []
            | y > length g - 1  = []
            | otherwise         = nX (x - 1) y <> nX x y <> nX (x + 1) y
 
-neighborSeats :: Int -> Int -> Grid -> [Char]
-neighborSeats x y g = catMaybes [ fstE , fstW , fstN , fstS
-                                , fstNE, fstNW, fstSE, fstSW ]
--- neighborSeats x y g = catMaybes [ fstNE ]
+findSeat :: Int -> Int -> Grid -> Int -> Int -> Maybe Char
+findSeat x y g xOffs yOffs = findSeat' (x + xOffs) (y + yOffs)
   where
-    fstW  = sLast . notEmpty . take (x - 1) $ g !! y
-    fstE  = sHead . notEmpty . drop (x + 1) $ g !! y
-    fstN  = sLast . notEmpty . map (!! x) . take (y - 1) $  g
-    fstS  = sHead . notEmpty . map (!! x) . drop (y + 1) $  g
-    fstNE = sHead . notEmpty
-            $ [ g !! c !! r | c <- [y-1..0], c > 0 | r <- [x+1..length (head g) - 1]]
-    fstNW = sHead . notEmpty
-            $ [ g !! c !! r | c <- [y-1..0], c > 0 | r <- [x-1..0], r > 0 ]
-    fstSE = sHead . notEmpty
-            $ [ g !! c !! r | c <- [y+1..length g - 1] | r <- [x+1..length (head g) - 1]]
-    fstSW = sHead . notEmpty
-            $ [ g !! c !! r | c <- [y+1..length g - 1] | r <- [x-1..0], r > 0]
-    notEmpty = filter (/= '.')
+    findSeat' x y
+      | x > length (g ! 0) - 1 || x < 0 || y > length g - 1 || y < 0 = Nothing
+      | g ! y ! x /= '.' = Just $ g ! y ! x
+      | otherwise = findSeat' (x + xOffs) (y + yOffs)
+
+neighborSeats :: Int -> Int -> Grid -> [Char]
+neighborSeats x y g = catMaybes [ findSeat x y g x' y'
+                                | x' <- [-1..1], y' <- [-1..1]
+                                , not (x' == y' && x' == 0) ]
 
 occupiedAdj :: Int -> Int -> Grid -> Int
 occupiedAdj x y g = length . filter (== '#') $ neighbors x y g
 
+occupiedAdjSeats :: Int -> Int -> Grid -> Int
+occupiedAdjSeats x y g = length . filter (== '#') $ neighborSeats x y g
+
 change :: Int -> Int -> Grid -> Char
-change x y g = case g !! y !! x of
+change x y g = case g ! y ! x of
   '.' -> '.'
   'L' | occupiedAdj x y g == 0 -> '#'
       | otherwise              -> 'L'
   '#' | occupiedAdj x y g >= 4 -> 'L'
   '#' | otherwise              -> '#'
 
-step :: Grid -> Grid
-step g = [ [ change x y g | x <- [0..length (head g) - 1]]
-           | y <- [0..length g - 1] ]
+change2 :: Int -> Int -> Grid -> Char
+change2 x y g = case g ! y ! x of
+  '.' -> '.'
+  'L' | occupiedAdjSeats x y g == 0 -> '#'
+      | otherwise                   -> 'L'
+  '#' | occupiedAdjSeats x y g >= 5 -> 'L'
+  '#' | otherwise                   -> '#'
 
-stepTillStable :: Grid -> Grid
-stepTillStable g
+step :: (Int -> Int -> Grid -> Char) -> Grid -> Grid
+step change g = listArray (bounds g) [ listArray (bounds $ g ! 0) [ change x y g | x <- [0..length (g ! 0) - 1]] | y <- [0..length g - 1] ]
+
+stepTillStable :: (Int -> Int -> Grid -> Char) -> Grid -> Grid
+stepTillStable change g
   | newGrid == g = g
-  | otherwise    = stepTillStable newGrid
-  where newGrid = step g
+  | otherwise    = stepTillStable change newGrid
+  where newGrid = step change g
 
 star1 :: Grid -> Int
-star1 = length . filter (== '#') . concat . stepTillStable
+star1 = length . filter (== '#') . concatMap elems . elems
+        . stepTillStable change
+
+star2 :: Grid -> Int
+star2 = length . filter (== '#') . concatMap elems . elems
+        . stepTillStable change2
 
 main :: IO ()
 main = do
   putStrLn . ("Star 1: " <>) . show . star1 =<< input
-    where input = lines <$> readFile "input"
+  putStrLn . ("Star 2: " <>) . show . star2 =<< input
+    -- where input = lines <$> readFile "input"
+    where input = do
+          raw <- lines <$> readFile "input"
+          pure $ listArray (0, length raw - 1)
+            $ map (listArray (0, length (head raw) - 1)) raw
+
+
 
 g = lines <$> readFile "testGrid"
 n = lines <$> readFile "testNone"
